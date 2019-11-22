fa17559060da2bb60036e6dca35b351d53c617cc
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 19:05:37 +0100

    Added roles and policies constants

---
+++ srv/EasyParking/EasyParking/Auth/Constants/Policies.cs
@@ -0,0 +1,7 @@
+﻿namespace EasyParking.Auth.Constants
+{
+    public static class Policies
+    {
+        public const string Admin = "admin";
+    }
+}
---
+++ srv/EasyParking/EasyParking/Auth/Constants/Roles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace EasyParking.Auth.Constants
+{
+    public static class Roles
+    {
+        public const string Admin = "admin";
+
+        public static readonly IReadOnlyCollection<string> RolesCollection
+            = typeof(Roles).GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Where(f => f.IsLiteral && !f.IsInitOnly)
+                .Select(f => (string)f.GetRawConstantValue())
+                .ToList();
+    }
+}
