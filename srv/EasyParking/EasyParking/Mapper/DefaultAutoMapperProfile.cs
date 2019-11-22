9faad6728125eccc72cfab3e8e34a0670ef75b3f
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 19:05:52 +0100

    Added auth models and mappings

---
+++ srv/EasyParking/EasyParking/Mapper/DefaultAutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EasyParking.Users.Dto;
+using EasyParking.Users.Models;
+using EasyParking.Auth.Params;
+using EasyParking.Mapper.Resolvers;
+
+namespace EasyParking.Mapper
+{
+    public class DefaultAutoMapperProfile : Profile
+    {
+        public DefaultAutoMapperProfile()
+        {
+            CreateMap<UserAccount, UserAccountDto>();
+
+            CreateMap<RegisterParams, UserAccount>()
+                .ForMember(u => u.PasswordHash, mce => mce.MapFrom<RegisterParamsPasswordHashResolver>());
+        }
+    }
+}
---
+++ srv/EasyParking/EasyParking/Mapper/Resolvers/RegisterParamsPasswordHashResolver.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using EasyParking.Users.Models;
+using EasyParking.Auth.Params;
+using EasyParking.Auth.Services;
+
+namespace EasyParking.Mapper.Resolvers
+{
+    public class RegisterParamsPasswordHashResolver : IValueResolver<RegisterParams, UserAccount, string>
+    {
+        private readonly IHashService hashService;
+
+        public RegisterParamsPasswordHashResolver(IHashService hashService)
+        {
+            this.hashService = hashService;
+        }
+
+        public string Resolve(RegisterParams source, UserAccount destination, string destMember, ResolutionContext context)
+        {
+            return hashService.Hash(source.Password);
+        }
+    }
+}
