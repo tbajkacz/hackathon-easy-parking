f8dcc64d1a2d45461c8469c7c959d3844c03f102
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 19:29:21 +0100

    Updated api routes

--- web/easy-parking/src/modules/auth/authService.ts
+++ web/easy-parking/src/modules/auth/authService.ts
@@ -1,22 +1,30 @@
-import axios, { AxiosPromise } from "axios";
+import axios from "axios";
 import { CurrentUser, SignInCredentials } from "./authTypes";
+import { unwrap } from "../../common/serviceUtility";
+import { ApiResponse } from "../../common/types";
 
-const signIn = (params?: SignInCredentials): AxiosPromise<CurrentUser> => {
-  return axios.post<CurrentUser>("http://localhost:5000/auth/signIn", {
-    params
-  });
+const signIn = (
+  params?: SignInCredentials
+): Promise<ApiResponse<CurrentUser>> => {
+  return axios
+    .post<ApiResponse<CurrentUser>>("auth/SignIn", {
+      params
+    })
+    .then(unwrap);
 };
 
-const signUp = (): AxiosPromise<CurrentUser> => {
-  return axios.post<CurrentUser>("http://localhost:5000/auth/signUp");
+const signUp = (): Promise<ApiResponse<CurrentUser>> => {
+  return axios.post<ApiResponse<CurrentUser>>("auth/Register").then(unwrap);
 };
 
 const signOut = () => {
-  return axios.post("");
+  return axios.post<ApiResponse<undefined>>("auth/SignOut").then(unwrap);
 };
 
-const getCurrentUser = (): AxiosPromise<CurrentUser> => {
-  return axios.get<CurrentUser>("http://...");
+const getCurrentUser = (): Promise<ApiResponse<CurrentUser>> => {
+  return axios
+    .get<ApiResponse<CurrentUser>>("auth/GetCurrentUser")
+    .then(unwrap);
 };
 
 const authService = {
