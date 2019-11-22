9d24cbd9c120a29df8d059701ef14405593a063b
Rafal Labisz <rafal0710@gmail.com>
2019-11-22 19:00:16 +0100

    add routing to app

--- web/easy-parking/src/App.tsx
+++ web/easy-parking/src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./App.scss";
+import { ProvideAuth } from "./modules/auth/authContext";
+import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
+import { routes } from "./routes";
+import SignIn from "./modules/auth/SignIn";
+import SignUp from "./modules/auth/SignUp";
+import Reservation from "./modules/domain/reservation/Reservation";
+import SettingsPanel from "./modules/domain/settingsPanel/SettingsPanel";
 
 const App: React.FC = () => {
-  return <div className="app">OK</div>;
+  return (
+    <ProvideAuth>
+      <HashRouter>
+        <Switch>
+          <Route path={routes.login} component={SignIn} />
+          <Route path={routes.register} component={SignUp} />
+          <Route exact path={routes.main} component={Reservation} />
+          <Route exact path={routes.settings} component={SettingsPanel} />
+          <Redirect to={routes.login} />
+        </Switch>
+      </HashRouter>
+    </ProvideAuth>
+  );
 };
 
 export default App;
