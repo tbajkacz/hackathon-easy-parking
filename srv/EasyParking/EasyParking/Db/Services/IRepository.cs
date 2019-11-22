3397ffa13056b9720300446258f1d87b7de5b5f2
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 18:58:06 +0100

    Added repository interfaces and base implementations

---
+++ srv/EasyParking/EasyParking/Db/Services/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using EasyParking.Db.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EasyParking.Db.Services
+{
+    public interface IRepository<TEntity, TId> where TEntity : Entity<TId>
+    {
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(TId id);
+
+        Task<TEntity> GetByIdAsync(TId id);
+
+        TEntity GetByIdOrDefault(TId id);
+
+        Task<TEntity> GetByIdOrDefaultAsync(TId id);
+
+        Task AddAsync(TEntity entity);
+
+        Task UpdateAsync(TEntity entity);
+
+        Task DeleteAsync(TId id);
+    }
+}
---
+++ srv/EasyParking/EasyParking/Db/Services/NHibernateUnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using NHibernate;
+using System.Threading.Tasks;
+
+namespace EasyParking.Db.Services
+{
+    public class NHibernateUnitOfWork : IUnitOfWork
+    {
+        private readonly ISession session;
+        private readonly ITransaction transaction;
+
+        public NHibernateUnitOfWork(ISession session)
+        {
+            this.session = session;
+            transaction = session.BeginTransaction();
+        }
+
+        public async Task CommitAsync()
+        {
+            await transaction.CommitAsync();
+        }
+
+        public async Task RollbackAsync()
+        {
+            await transaction.RollbackAsync();
+        }
+
+        public void Dispose()
+        {
+            if (transaction != null)
+            {
+                transaction.Dispose();
+            }
+            session.Dispose();
+        }
+    }
+}
