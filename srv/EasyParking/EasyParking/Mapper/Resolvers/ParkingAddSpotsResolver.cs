c3325c48c0f704a3db9a93188154abb71b4ea589
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 22:52:40 +0100

    Added missing resolvers for parking models mappings

---
+++ srv/EasyParking/EasyParking/Mapper/Resolvers/ParkingAddSpotsResolver.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EasyParking.Parkings.Models;
+using EasyParking.Parkings.Params;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EasyParking.Mapper.Resolvers
+{
+    public class ParkingAddSpotsResolver : IValueResolver<ParkingAddParams, Parking, IList<ParkingSpot>>
+    {
+        public IList<ParkingSpot> Resolve(ParkingAddParams source, Parking destination, IList<ParkingSpot> destMember, ResolutionContext context)
+        {
+            int index = 1;
+            return Enumerable.Range(0, source.ParkingSpotsAmount)
+                .Select(_ => new ParkingSpot { SpotNumber = index++, Reservations = new List<Reservation>() })
+                .ToList();
+        }
+    }
+}
