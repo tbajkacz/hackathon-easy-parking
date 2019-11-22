a05481e6cfe3a9d4148b881549d3f210c91ceedf
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-23 00:34:52 +0100

    Added utc time getters

--- srv/EasyParking/EasyParking/Parkings/Models/Reservation.cs
+++ srv/EasyParking/EasyParking/Parkings/Models/Reservation.cs
@@ -8,11 +8,21 @@ namespace EasyParking.Parkings.Models
     {
         public virtual UserAccount ReservedBy { get; set; }
 
+        /// <summary>
+        /// May provide incorrect hour due to db conversions use <see cref="FromUtc"/> instead
+        /// </summary>
         public virtual DateTime ReservedFrom { get; set; }
 
+        /// <summary>
+        /// May provide incorrect hour due to db conversions use <see cref="UntilUtc"/> instead
+        /// </summary>
         public virtual DateTime ReservedUntil { get; set; }
 
         public virtual ParkingSpot ReservedSpot { get; set; }
+
+        public virtual DateTime FromUtc => ReservedFrom.ToUniversalTime();
+
+        public virtual DateTime UntilUtc => ReservedUntil.ToUniversalTime();
     }
 
     public class ReservationMap : EntityMap<Reservation, int>
