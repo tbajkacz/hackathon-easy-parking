2bd7d343a90f470593fbbc4ab03f4a92ef6a61fc
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 18:58:16 +0100

    Added missing exception

---
+++ srv/EasyParking/EasyParking/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyParking.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(Type entityType, object id)
+            : base($"Entity of type {entityType.FullName} with id {id} was not found")
+        {
+        }
+    }
+}
