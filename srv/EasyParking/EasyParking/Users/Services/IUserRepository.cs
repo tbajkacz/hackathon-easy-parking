8e33992a51843d8939cd19fbca732d3087bf3f3b
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 18:58:58 +0100

    Added user repository

---
+++ srv/EasyParking/EasyParking/Users/Services/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasyParking.Users.Models;
+using EasyParking.Auth.Params;
+using EasyParking.Db.Services;
+using System.Threading.Tasks;
+
+namespace EasyParking.Users.Services
+{
+    public interface IUserRepository : IRepository<UserAccount, int>
+    {
+        Task<UserAccount> ValidateCredentialsAsync(AuthParams param);
+    }
+}
---
+++ srv/EasyParking/EasyParking/Users/Services/NHibernateUserRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using EasyParking.Users.Models;
+using NHibernate;
+using NHibernate.Linq;
+using EasyParking.Auth.Params;
+using EasyParking.Auth.Services;
+using EasyParking.Db.Services;
+
+namespace EasyParking.Users.Services
+{
+    public class NHibernateUserRepository : NHibernateRepositoryBase<UserAccount, int>, IUserRepository
+    {
+        private readonly IHashService hashService;
+
+        public NHibernateUserRepository(ISession session, IHashService hashService)
+            : base(session)
+        {
+            this.hashService = hashService;
+        }
+
+        public async Task<UserAccount> ValidateCredentialsAsync(AuthParams param)
+        {
+            var user = await session.Query<UserAccount>()
+                .SingleOrDefaultAsync(u => u.Login == param.Login);
+
+            return user == null ? null : hashService.CompareHashes(param.Password, user.PasswordHash) ? user : null;
+        }
+    }
+}
