1bab3d2a746c8e47c9fe954850de62e66ad2c0f1
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 19:06:46 +0100

    Configured middleware pipeline and services

--- srv/EasyParking/EasyParking/Startup.cs
+++ srv/EasyParking/EasyParking/Startup.cs
@@ -1,23 +1,57 @@
+using AutoMapper;
+using AutoWrapper;
+using EasyParking.Auth.Services;
+using EasyParking.Extensions;
+using EasyParking.Mapper;
+using EasyParking.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace EasyParking
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            this.env = env;
         }
 
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<HashOptions>(Configuration.GetSection("Hash"));
+
             services.AddControllers();
+
+            services.AddNHibernateSessionFactory(Configuration.GetConnectionString("Db"));
+
+            services.AddNHibernateRepositories();
+
+            services.AddCookieAuthentication(env.IsDevelopment() ? CookieSecurePolicy.None : CookieSecurePolicy.Always);
+            services.AddAuthorizationWithPolicies();
+
+            services.AddSingleton<IHashService, HashService>();
+
+            services.AddAutoMapper(cfg => cfg.AddProfile<DefaultAutoMapperProfile>(), Assembly.GetAssembly(typeof(Startup)));
+
+            services.AddSwaggerGen(cfg =>
+            {
+                cfg.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "RSRL",
+                    Version = "1.0",
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -27,10 +61,22 @@ namespace EasyParking
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Api");
+            });
+
+            app.UseSwagger();
+
+            app.UseApiResponseAndExceptionWrapper(new AutoWrapperOptions
+            {
+                IsDebug = env.IsDevelopment(),
+                ShowStatusCode = true
+            });
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
