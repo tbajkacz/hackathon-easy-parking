8aecfd035c0c3ed8cde501947c466e66a0fe5f6c
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 22:51:27 +0100

    Added missing parking params

---
+++ srv/EasyParking/EasyParking/Parkings/Params/ParkingAddParams.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyParking.Parkings.Params
+{
+    public class ParkingAddParams
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public long PricePerHour { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public int ParkingSpotsAmount { get; set; }
+    }
+}
