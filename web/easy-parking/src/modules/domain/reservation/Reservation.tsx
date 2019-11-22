3ab8c5089b5b7748a71fdf0ed0e11921e3ea25f0
Rafal Labisz <rafal0710@gmail.com>
2019-11-22 19:01:25 +0100

    add settings and reservation panel

---
+++ web/easy-parking/src/modules/domain/reservation/Reservation.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainTemplate from "../../../templates/MainTemplate";
+
+interface ReservationProps {}
+
+const Reservation: React.FC<ReservationProps> = props => {
+  return (
+    <MainTemplate>
+      <div>RESERVATION</div>
+    </MainTemplate>
+  );
+};
+export default Reservation;
---
+++ web/easy-parking/src/modules/domain/settingsPanel/SettingsPanel.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainTemplate from "../../../templates/MainTemplate";
+
+interface SettingsPanelProps {}
+
+const SettingsPanel: React.FC<SettingsPanelProps> = props => {
+  return (
+    <MainTemplate>
+      <div>SETTINGS</div>
+    </MainTemplate>
+  );
+};
+export default SettingsPanel;
