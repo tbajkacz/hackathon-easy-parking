2ef361ed1a58ffbf81fffa84998d99929f48f2c0
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 18:58:31 +0100

    Added Id convention for nhibernate

---
+++ srv/EasyParking/EasyParking/Db/Conventions/IncrementIdConvention.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.Instances;
+using System;
+
+namespace EasyParking.Db.Conventions
+{
+    public class IncrementIdConvention : IIdConvention
+    {
+        public void Apply(IIdentityInstance instance)
+        {
+            if (!instance.Type.Name.Contains("string", StringComparison.InvariantCultureIgnoreCase))
+            {
+                instance.GeneratedBy.Increment();
+            }
+        }
+    }
+}
