fd42bb29e2af46567a9a48f97d12896fa5777e32
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 19:05:03 +0100

    Added extension method to simplify signing in

---
+++ srv/EasyParking/EasyParking/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,21 @@
+﻿using EasyParking.Users.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using EasyParking.Extensions;
+using System.Threading.Tasks;
+
+namespace EasyParking.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static async Task SignInAsync(this HttpContext httpContext, UserAccount user, bool rememberMe)
+            => await httpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                                             user.GenerateClaims(),
+                                             new AuthenticationProperties
+                                             {
+                                                 AllowRefresh = true,
+                                                 IsPersistent = rememberMe,
+                                             });
+    }
+}
