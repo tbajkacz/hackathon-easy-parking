a1bd365f172b31070c57d505b8883a02d83467f3
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-22 18:57:20 +0100

    Added user models

---
+++ srv/EasyParking/EasyParking/Users/Dto/UserAccountDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace EasyParking.Users.Dto
+{
+    public class UserAccountDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public long Balance { get; set; }
+
+        public string Login { get; set; }
+
+        public IList<string> Roles { get; set; }
+    }
+}
---
+++ srv/EasyParking/EasyParking/Users/Models/UserAccount.cs
@@ -0,0 +1,44 @@
+﻿using EasyParking.Db.Models;
+using System.Collections.Generic;
+
+namespace EasyParking.Users.Models
+{
+    public class UserAccount : Entity<int>
+    {
+        public virtual string Name { get; set; }
+
+        public virtual string Surname { get; set; }
+
+        public virtual string Email { get; set; }
+
+        public virtual string Login { get; set; }
+
+        public virtual string PasswordHash { get; set; }
+
+        public virtual long Balance { get; set; } = 0;
+
+        public virtual IList<string> Roles { get; set; } = new List<string> { };
+    }
+
+    public class UserAccountMap : EntityMap<UserAccount, int>
+    {
+        public UserAccountMap()
+        {
+            Map(x => x.Name)
+                .Not.Nullable();
+            Map(x => x.Surname)
+                .Not.Nullable();
+            Map(x => x.Email)
+                .Not.Nullable();
+            Map(x => x.Login)
+                .Not.Nullable()
+                .Unique();
+            Map(x => x.PasswordHash)
+                .Not.Nullable();
+            Map(x => x.Balance);
+            HasMany(x => x.Roles)
+                .Table("Roles")
+                .Element("Role");
+        }
+    }
+}
