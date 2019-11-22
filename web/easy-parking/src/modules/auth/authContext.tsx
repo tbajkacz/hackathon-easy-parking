cfd498355e289fb7e3afa499a0492d799610383a
Rafal Labisz <rafal0710@gmail.com>
2019-11-22 18:58:52 +0100

    add authContext to handle authorization user

---
+++ web/easy-parking/src/modules/auth/authContext.tsx
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect, useState } from "react";
+import authService from "./authService";
+// import LoadingIndicator from "../LoadingIndicator";
+import { SignInCredentials, CurrentUser, Auth } from "./authTypes";
+
+export const AuthContext = React.createContext<Auth>({
+  signIn: () => null,
+  signOut: () => null
+});
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+interface ProvideAuthProps {
+  children: React.ReactNode;
+}
+
+export function ProvideAuth(props: ProvideAuthProps) {
+  const { Provider } = AuthContext;
+  const { promise, ...auth } = useProvideAuth();
+  return (
+    // <LoadingIndicator promise={promise}>
+    // <Provider value={auth}>{props.children}</Provider>
+    <Provider value={auth}>{props.children}</Provider>
+    // </LoadingIndicator>
+  );
+}
+
+export function useProvideAuth() {
+  const [currentUser, setCurrentUser] = useState<CurrentUser>();
+  const [promise, setPromise] = useState<Promise<any> | undefined>();
+
+  const signIn = (params?: SignInCredentials) => {
+    authService.signIn(params).then(r => {
+      setPromise(
+        authService.getCurrentUser().then(r => {
+          // setCurrentUser(r.result);
+        })
+      );
+    });
+  };
+
+  const signOut = () => {
+    setPromise(authService.signOut());
+    setCurrentUser(undefined);
+  };
+
+  useEffect(() => {
+    setPromise(
+      authService.getCurrentUser().then(user => {
+        // setCurrentUser(user.result);
+      })
+    );
+  }, []);
+
+  return { currentUser, signIn, signOut, promise };
+}
